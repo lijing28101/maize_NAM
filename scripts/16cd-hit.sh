c85b77071fa511fd1021d1d6cc4a9a1c92d5f625
lijing28101 <jingli@iastate.edu>
2020-07-13 14:58:26 -0500

    Update 16cd-hit.sh

--- scripts/16cd-hit.sh
+++ scripts/16cd-hit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+module load cufflinks
+module load emboss
 module load genometools
 
+gffread mikado.loci.gff3 -g Zm-B73-REFERENCE-NAM-5.0.fa -y pep.fa
+sed -i 's/\.$/*/' pep.fa
 /opt/rit/spack-app/linux-rhel7-x86_64/gcc-4.8.5/transdecoder-3.0.1-53ns2nerx6pvd23gs4rhc35636r53x6l/util/bin/cd-hit -i pep.fa -T 0 -c 1 -M 0 -d 0 -o filter.pep.fa
 grep ">" filter.pep.fa | cut -f1 -d" " | sed 's/>//' | sed 's/..$//' | sort > nr-trans.txt
 cut -f1-2 -d"." nr-trans.txt | sort -u > nr-gene.txt
