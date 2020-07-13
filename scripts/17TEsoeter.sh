93bfa3da638823fc7024267ee7ef3abcfdc28c68
lijing28101 <jingli@iastate.edu>
2020-07-13 14:59:20 -0500

    Update 17TEsoeter.sh

--- scripts/17TEsoeter.sh
+++ scripts/17TEsoeter.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-module load cufflinks
-module load emboss
-
-gffread mikado.loci.gff3 -g Zm-B73-REFERENCE-NAM-5.0.fa -x cds.fa
-transeq -sequence cds.fa -outseq pep.fa
 TEsorter filter.pep.fa -p 64 -st prot
 grep -v "^#" filter.pep.fa.rexdb.cls.tsv | cut -f 1 | sort | uniq |sed 's/..$//' | sort > TE-trans.txt
 
