29ac6ee3b001cb8012ecd959ced5c7801b78e2c8
lijing28101 <jingli@iastate.edu>
2022-02-14 19:21:58 -0600

    Update 1salmon_index.sh

--- downstream_analysis/1salmon_index.sh
+++ downstream_analysis/1salmon_index.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# filter.cds.fa is the CDS sequence of non-redundant trascripts.
-salmon index -t filter.cds.fa -p 96 -i salmon_index
+# filter.cds.fa is the cDNA sequence of non-redundant trascripts.
+cat filter.cdna.fa Zm-B73-REFERENCE-NAM-5.0.fa> B73.gentrome.fa
+salmon index -t B73.gentrome.fa -p 96 -i salmon_index --keepDuplicates
